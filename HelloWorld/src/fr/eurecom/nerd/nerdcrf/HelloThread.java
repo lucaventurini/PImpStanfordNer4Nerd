399da28e327df8f4d77275d33d00c6920e6caf32
lucaventurini <luca.venturini@eurecom.fr>
2013-04-03 12:22:12 +0200

    Thread demo

--- HelloWorld/src/fr/eurecom/nerd/nerdcrf/HelloThread.java
+++ HelloWorld/src/fr/eurecom/nerd/nerdcrf/HelloThread.java
@@ -12,9 +12,13 @@ public class HelloThread {
 	 */
 	public static void main(String[] args) throws ClassCastException, IOException, ClassNotFoundException {
 		MyClassifier cl = new MyClassifier("classifiers/english.muc.7class.distsim.crf.ser.gz", "liberty.txt", "out_7class.txt");
+		//MyClassifier cl = new MyClassifier("classifiers/english.muc.7class.distsim.crf.ser.gz", "la_divin.txt", "out_7class.txt");
+
 		Thread t = new Thread(cl);
 		
 		MyClassifier cl2 = new MyClassifier("classifiers/english.all.3class.distsim.crf.ser.gz", "liberty.txt", "out_3class.txt");
+		//MyClassifier cl2 = new MyClassifier("classifiers/english.all.3class.distsim.crf.ser.gz", "la_divin2.txt", "out_3class.txt");
+
 		Thread t2 = new Thread(cl2);
 		
 		t.start();
